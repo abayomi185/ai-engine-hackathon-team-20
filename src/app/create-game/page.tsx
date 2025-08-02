9c73417fcde45e7922405536b856c4e57feed521
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-02 20:17:56 +0100

    fix: create game page and remove suspense

--- src/app/create-game/page.tsx
+++ src/app/create-game/page.tsx
@@ -1,27 +1,26 @@
 "use client";
 
-import { Suspense } from "react";
-
 import { QRCodeSVG } from "qrcode.react";
 import { api } from "~/trpc/react";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3002";
 
-function QRCodeSection() {
-  const newGame = api.game.new.useQuery(undefined, { suspense: true });
-  const gameId = newGame.data?.id ?? "";
+const QRCodeSection = () => {
+  const { data, isLoading, error } = api.game.new.useQuery();
+  const gameId = data?.id ?? "";
   const qrValue = gameId ? `${baseUrl}/game/${gameId}` : "";
 
+  if (isLoading) return <div>Loading QR Code...</div>;
+  if (error || !data?.id) return <div>Failed to load QR Code.</div>;
+
   return <QRCodeSVG value={qrValue} size={180} />;
-}
+};
 
 export default function Results() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
       <div className="w-full max-w-xs">
-        <Suspense fallback={<div>Loading QR Code...</div>}>
-          <QRCodeSection />
-        </Suspense>
+        <QRCodeSection />
       </div>
     </div>
   );
