a277575aaf9c7961f6bd7b0009607c97daa3f59e
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-02 15:44:17 +0100

    feat: update migrations

--- drizzle/0000_careful_ma_gnuci.sql
+++ drizzle/0000_careful_ma_gnuci.sql
@@ -7,7 +7,7 @@ CREATE TABLE "test_game" (
 --> statement-breakpoint
 CREATE TABLE "test_game_round" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"gameId" integer NOT NULL,
+	"gameId" uuid NOT NULL,
 	"roundNumber" integer NOT NULL,
 	"createdAt" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	"content" text NOT NULL
@@ -22,7 +22,7 @@ CREATE TABLE "test_post" (
 --> statement-breakpoint
 CREATE TABLE "test_session" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"gameId" integer,
+	"gameId" uuid,
 	"isPlayer" boolean DEFAULT false NOT NULL,
 	"avatar" text DEFAULT '' NOT NULL,
 	"createdAt" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -30,8 +30,8 @@ CREATE TABLE "test_session" (
 --> statement-breakpoint
 CREATE TABLE "test_submission" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"sessionId" integer NOT NULL,
-	"gameRound" integer NOT NULL,
+	"sessionId" uuid NOT NULL,
+	"gameRound" uuid NOT NULL,
 	"content" text NOT NULL,
 	"createdAt" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
@@ -43,6 +43,12 @@ CREATE TABLE "test_vote" (
 	"voteValue" integer NOT NULL
 );
 --> statement-breakpoint
+ALTER TABLE "test_game_round" ADD CONSTRAINT "test_game_round_gameId_test_game_id_fk" FOREIGN KEY ("gameId") REFERENCES "public"."test_game"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "test_session" ADD CONSTRAINT "test_session_gameId_test_game_id_fk" FOREIGN KEY ("gameId") REFERENCES "public"."test_game"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "test_submission" ADD CONSTRAINT "test_submission_sessionId_test_session_id_fk" FOREIGN KEY ("sessionId") REFERENCES "public"."test_session"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "test_submission" ADD CONSTRAINT "test_submission_gameRound_test_game_round_id_fk" FOREIGN KEY ("gameRound") REFERENCES "public"."test_game_round"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "test_vote" ADD CONSTRAINT "test_vote_sessionId_test_session_id_fk" FOREIGN KEY ("sessionId") REFERENCES "public"."test_session"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "test_vote" ADD CONSTRAINT "test_vote_gameRoundId_test_game_round_id_fk" FOREIGN KEY ("gameRoundId") REFERENCES "public"."test_game_round"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "game_name_idx" ON "test_game" USING btree ("name");--> statement-breakpoint
 CREATE INDEX "game_round_game_idx" ON "test_game_round" USING btree ("gameId");--> statement-breakpoint
 CREATE INDEX "name_idx" ON "test_post" USING btree ("name");--> statement-breakpoint
