e43373cefcee665a82275a2051afd05674c74668
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-02 13:48:58 +0100

    feat: update db schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,28 +6,40 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 
-export const user = pgTable("user", {
-  id: integer("id").primaryKey(),
-  name: text("name").notNull(),
-});
-
 export const game = pgTable("game", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
-  description: text("description").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
   isActive: boolean("is_active").notNull().default(true),
+});
+
+export const gameRound = pgTable("game_round", {
+  id: integer("id").primaryKey(),
+  gameId: integer("game_id").notNull(),
+  roundNumber: integer("round_number").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  initialPrompt: text("initial_prompt").notNull(),
 });
 
 export const session = pgTable("session", {
   id: integer("id").primaryKey(),
-  userId: integer("user_id").notNull(),
-  gameId: integer("game_id").notNull(),
-  isActive: boolean("is_active").notNull().default(true),
+  gameId: integer("game_id"),
+  isPlayer: integer("is_player").notNull().default(0),
+  avatar: text("avatar").notNull().default(""),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const submission = pgTable("submission", {
+  id: integer("id").primaryKey(),
+  sessionId: integer("session_id").notNull(),
+  gameRound: integer("game_round").notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
 
 export const vote = pgTable("vote", {
   id: integer("id").primaryKey(),
   sessionId: integer("session_id").notNull(),
+  gameRoundId: integer("game_round_id").notNull(),
   voteValue: integer("vote_value").notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
