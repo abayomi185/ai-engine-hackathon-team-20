56946760b073bb9483d1b197ee7e95be4225afe2
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-02 16:19:49 +0100

    feat: update new and vote endpoint in game router

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -1,18 +1,23 @@
 import { z } from "zod";
-import { v4 as uuidv4 } from "uuid";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { game, session, gameRound, vote, submission } from "~/server/db/schema";
 import { and, eq } from "drizzle-orm";
 
 export const gameRouter = createTRPCRouter({
-  new: publicProcedure.query(async ({ ctx, input }) => {}),
+  new: publicProcedure.query(async ({ ctx }) => {
+    const newGame = await ctx.db.insert(game).values({
+      name: "",
+      currentGameRound: 0,
+    });
+
+    const { id } = newGame[0]!;
+    return { id };
+  }),
 
   join: publicProcedure
     .input(z.object({ gameId: z.string() }))
     .query(async ({ ctx, input }) => {
-      const userId = uuidv4();
-
       const playerSessions = await ctx.db.query.session.findMany({
         where: and(
           eq(session.gameId, input.gameId),
@@ -49,14 +54,30 @@ export const gameRouter = createTRPCRouter({
           where: eq(session.id, sessionId),
         });
 
-        if (!sessionExists) {
-          throw new Error("Session not found");
+        if (!sessionExists?.gameId) {
+          throw new Error("Session not found or does not have a gameId");
         }
 
         // Check if game is active
-        // const gameExists = await tx.query.game.findFirst({
-        //   where: eq(game.id, sessionExists.gameId),
-        // });
+        const gameInstance = await tx.query.game.findFirst({
+          where: eq(game.id, sessionExists.gameId),
+        });
+
+        if (!gameInstance?.isActive) {
+          throw new Error("Game is not active");
+        }
+
+        await tx.insert(vote).values({
+          sessionId: sessionId,
+          gameRoundId: gameInstance.id,
+          voteValue: input.voteValue,
+        });
+
+        // Increment the current game round
+        await tx
+          .update(game)
+          .set({ currentGameRound: gameInstance.currentGameRound + 1 })
+          .where(eq(game.id, sessionExists.gameId));
       });
     }),
   // getResults: publicProcedure.query(async ({ ctx }) => {}),
