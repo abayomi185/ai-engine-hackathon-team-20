eba144007905b37ebc03732c177af2ecc66ce683
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-03 11:16:44 +0100

    feat: add AlertModal component

---
+++ src/components/AlertModal.tsx
@@ -0,0 +1,41 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "~/components/ui/alert-dialog";
+
+type AlertModalProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title?: string;
+  description: string;
+  actionLabel?: string;
+};
+
+export function AlertModal({
+  open,
+  onOpenChange,
+  title = "Alert",
+  description,
+  actionLabel = "OK",
+}: AlertModalProps) {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction onClick={() => onOpenChange(false)}>
+            {actionLabel}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
